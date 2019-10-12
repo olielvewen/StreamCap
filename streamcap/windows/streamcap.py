acc723216ac749eee322e53ca00212536bb07bb6
olielvewen <olivier@openshot.org>
2019-10-12 17:48:57 +0200

    introduce the license in the main file - at first look not already done ?

--- streamcap/windows/streamcap.py
+++ streamcap/windows/streamcap.py
@@ -1,6 +1,23 @@
 ﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+# StreamCap - A Graphical Tool for ripping DVD using vobcopy in backend
+# Copyright (c) 2015 Olivier Girard <olivier@openshot.org>
+#
+# StreamCap is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# StreamCap is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import sys
 import os
 import os.path
