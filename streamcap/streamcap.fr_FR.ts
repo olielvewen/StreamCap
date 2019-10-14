c2bc2ecdb11a4241b8c06dc333a0d44753a8659e
olielvewen <olivier@openshot.org>
2019-10-14 17:11:40 +0200

    resolution error in ts files - now files are full and functionnal

--- streamcap/streamcap.fr_FR.ts
+++ streamcap/streamcap.fr_FR.ts
@@ -1,3 +1,109 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>StreamCap</name>
+    <message>
+        <location filename="streamcap.py" line="130"/>
+        <source>Device Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="132"/>
+        <source>Device Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="134"/>
+        <source>Mount Point</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="136"/>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="147"/>
+        <source>Disc Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="149"/>
+        <source>Disc name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="150"/>
+        <source>Title count:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="158"/>
+        <source>Titles</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="166"/>
+        <source>Copy titles</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="180"/>
+        <source>Output Destination</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="183"/>
+        <source>Output Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="185"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="204"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="206"/>
+        <source>E&amp;xit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="207"/>
+        <source>&amp;Refresh Disc Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="218"/>
+        <source>StreamCap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="271"/>
+        <source>Choose output path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="308"/>
+        <source>Couldn&apos;t find mounted DVD</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VobCopyParser</name>
+    <message>
+        <location filename="streamcap.py" line="435"/>
+        <source>Couldn&apos;t find Vobcopy. Is it installed ?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="streamcap.py" line="438"/>
+        <source>Couldn&apos;t find Lsdvd. Is it installed ?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
